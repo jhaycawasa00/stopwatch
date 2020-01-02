9a289a2271c70ff2541e784cf61fbd6403c37bee
jhay <pipzhermogenes@gmail.com>
2020-01-02 14:27:38 +0800

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      seconds: 0,
+      paused: true,
+    }
+    this.timerId = null
+  }
+
+  handleStart(e){
+    if(!this.timerId){
+      this.timerId = setInterval( _ => this.updateTime(), 1000 )
+    }
+  }
+
+  handlePause(e){
+    let paused = !this.state.paused
+    if(paused){
+      this.resetInterval()
+      this.setState({
+        paused,
+      })
+    } else {
+      this.handleStart()
+    }
+  }
+
+  resetInterval(){
+    clearInterval(this.timerId)
+    this.timerId = null
+  }
+
+  handleReset(e){
+    this.resetInterval()
+    this.setState({
+      seconds: 0,
+      paused: true
+    })
+  }
+
+  updateTime(){
+    this.setState({
+      seconds: this.state.seconds + 1,
+      paused: false,
+    })
+  }
+
+  render() {
+    return (
+      <div className="stopwatch">
+        <h1>{this.state.seconds}</h1>
+        <div className="controls">
+          <button onClick={ e => this.handleReset(e) }>Reset</button>
+          <button onClick={ e => this.handleStart(e) }>Start</button>
+          <button onClick={ e => this.handlePause(e) }>Pause</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+/** WEBPACK FOOTER **
+ ** ./src/App.js
+ **/
